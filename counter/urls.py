b5c896173dcb50c35853e1a61895f05a48237745
Andrei Müller (Andy Miira) <andreiagmu@gmail.com>
2025-05-14 18:47:15 -0300

    Set up urls.py

---
+++ counter/urls.py
@@ -0,0 +1,10 @@
+﻿from django.urls import path
+
+from counter.views import get_counter, increment_counter, decrement_counter, reset_counter
+
+urlpatterns = [
+    path('get-counter/', get_counter, name='get-counter'),
+    path('increment-counter/', increment_counter, name='increment-counter'),
+    path('decrement-counter/', decrement_counter, name='decrement-counter'),
+    path('reset-counter/', reset_counter, name='reset-counter'),
+]
