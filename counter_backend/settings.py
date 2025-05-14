3674aabde4fd7734a5c8148108983aa4d9c52b01
Andrei Müller (Andy Miira) <andreiagmu@gmail.com>
2025-05-14 17:00:27 -0300

    settings.py: Add apps

--- counter_backend/settings.py
+++ counter_backend/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'counter.apps.CounterConfig',
+    'rest_framework',
+    'corsheaders',
+    'counter',
 ]
 
 MIDDLEWARE = [
