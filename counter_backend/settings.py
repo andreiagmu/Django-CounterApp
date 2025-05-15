70ee400323d11e605908d931b1015a3732bf98da
Andrei Müller (Andy Miira) <andreiagmu@gmail.com>
2025-05-15 12:56:41 -0300

    settings.py: Fix CORS settings

--- counter_backend/settings.py
+++ counter_backend/settings.py
@@ -50,7 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+# CORS settings
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'counter_backend.urls'
 
