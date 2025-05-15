9beef0c257bee2bc9d7af4c2f8fd6b133ab6ebce
Andrei Müller (Andy Miira) <andreiagmu@gmail.com>
2025-05-15 00:49:13 -0300

    App.jsx: Implement Counter app

--- counter_frontend/src/App.jsx
+++ counter_frontend/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counterValue, setCounterValue] = useState(50)
+
+  useEffect(() => {
+    axios.get('/api/get-counter/').then(res => setCounterValue(res.data.value))
+  }, []);
+
+  function handleIncrement() {
+    axios.post('/api/increment-counter/').then(res => setCounterValue(res.data.value))
+  }
+
+  function handleDecrement() {
+    axios.post('/api/decrement-counter/').then(res => setCounterValue(res.data.value))
+  }
+
+  function handleReset() {
+    axios.post('/api/reset-counter/').then(res => setCounterValue(res.data.value))
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='p-40 max-w-3xl mx-auto flex space-x-4'>
+        <button className='bg-blue-500 text-white p-4 rounded-lg' onClick={handleIncrement}>Increment</button>
+        <p className='border shadow-md max-w-fit p-6 rounded-lg text-2xl'>Counter Value: {counterValue}</p>
+        <button className='bg-red-500 text-white p-4 rounded-lg' onClick={handleDecrement}>Decrement</button>
+        <button className='bg-green-400 p-4 px-10 rounded-lg' onClick={handleReset}>Reset</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
